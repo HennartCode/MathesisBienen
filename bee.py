ce1403b700da1d85e6204cf361b8e94cdb26284e
Pavel <POWERPAVEL05>
2024-06-19 16:18:15 +0200

    +1

--- bee.py
+++ bee.py
@@ -7,7 +7,7 @@ class Bee(pygame.sprite.Sprite):
     NEUTRAL_COLOR = (255, 255, 255)
     ATTRACTED_COLOR = "red"
     RETURN_COLOR = "green"
-    SPEED = 5
+    speed = 5
     RADIUS = 100
     MAX_SPEED = 10 #m/s**2
 
@@ -21,8 +21,8 @@ class Bee(pygame.sprite.Sprite):
         self.rect.y = hive.rect.y
         self.dir = pygame.math.Vector2(dir_x, dir_y).normalize()
         #Placeholder
-        if (Bee.SPEED > Bee.MAX_SPEED):
-            Bee.SPEED = 10
+        if (Bee.speed > Bee.MAX_SPEED):
+            Bee.speed = BEE.MAX_SPEED
         """
             Speichert die Position der Biene als Float, wichtig für interne
             Verarbeitung, ['self.rect'] wird intern von pygame benutzt, dafür
@@ -51,7 +51,7 @@ class Bee(pygame.sprite.Sprite):
         # falls der Vektor zur Blume <= des Blumen Radius entspricht
         # und die Biene neutral ist, fliegt die Biene zur Blume
         if _len <= Bee.RADIUS and self.status == "neutral":
-            self.float_rect += to_flower/_len * min(_len, Bee.SPEED)
+            self.float_rect += to_flower/_len * min(_len, Bee.speed)
             self.image.fill(Bee.ATTRACTED_COLOR)
             
             # Ist sie nah genug dran, ändere den status und die Biene fliegt
