65ed7b3139dee32734f836e9214ea36e0d7e8ebf
NikolozKervalishvili <nikoloz.kervalishvili@gmx.de>
2024-07-27 18:31:47 +0200

    utils.py beschriftet

--- utils.py
+++ utils.py
@@ -1,6 +1,9 @@
 import config
+import pygame
 
-t = [
+# simuliert 8 weitere screens um den main screen herum (3x3 Feld)
+# hier sind alle Abstandsberechnungen vom main screen zu diesen 8 Feldern
+allDirections = [
     (0, 0),
     (-config.WIDTH, 0),
     (config.WIDTH, 0),
@@ -14,31 +17,27 @@ t = [
 
 # findet den kürzesten Vektor zwischen zwei Vektoren,
 # dabei wird das Simulationsfeld als Torus behandelt
-def nearest_vector(pos1, pos2):
+def nearestVector(pos1, pos2):
     """
         pos1: pygame.math.Vector2
         pos2: pygame.math.Vector2
         return: pygame.math.Vector2
     """
-    global t
-    # possible_vectors = [
-    #     pos2 - pos1,
-    #     pos2 - pygame.math.Vector2(pos1.x - config.WIDTH, pos1.y),
-    #     pos2 - pygame.math.Vector2(pos1.x + config.WIDTH, pos1.y),
-    #     pos2 - pygame.math.Vector2(pos1.x, pos1.y + config.HEIGHT),
-    #     pos2 - pygame.math.Vector2(pos1.x, pos1.y - config.HEIGHT),
-    #     pos2 - pygame.math.Vector2(pos1.x - config.WIDTH, pos1.y + config.HEIGHT),
-    #     pos2 - pygame.math.Vector2(pos1.x + config.WIDTH, pos1.y + config.HEIGHT),
-    #     pos2 - pygame.math.Vector2(pos1.x - config.WIDTH, pos1.y - config.HEIGHT),
-    #     pos2 - pygame.math.Vector2(pos1.x + config.WIDTH, pos1.y - config.HEIGHT),
-    # ]
-    # return min(possible_vectors, key = lambda v: v.length())
-
+    global allDirections
     smallest = pos2 - pos1
-    for x, y in t:
+    for x, y in allDirections:
         pos1.x += x
         pos1.y += y
         cur = pos2 - pos1
         if cur.length() < smallest.length():
             smallest = cur
     return smallest
+
+# nimmt einen tuple mit zwei floats und macht diesen zu einem Vector
+# error falls der tuple nicht genau floats (oder ints) hat
+def tupleToVector2(t):
+    """
+        t: (float, float)
+        return: pygame.math.Vector2
+    """
+    return pygame.math.Vector2(t[0], t[1])
